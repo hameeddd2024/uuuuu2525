64b856c2e5b33c49229c2f9e12ee5651af9cd352
hameeddd2024 <ht8563629@gmail.com>
2024-08-25 14:08:50 -0700

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,15 @@
+
+const mean = document.querySelector('.mean');
+const toggle = document.getElementById('toggle');
+
+toggle.onclick = function () {
+  mean.classList.toggle('active');
+}
+
+
+// start img
+
+function chanenImg(img) {
+  var singleImg = document.querySelector('#singleImg');
+  singleImg.setAttribute('src', img)
+}
